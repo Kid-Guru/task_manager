526dece3299903ef277117198db3112af61c0800
Kid-Guru <birkov.alexandr@gmail.com>
2021-04-27 11:51:44 +0400

    fix application.rb

--- config/application.rb
+++ config/application.rb
@@ -6,13 +6,14 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.paths << Rails.root.join('node_modules')
+
 
 module TaskManager
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.assets.paths << Rails.root.join('node_modules')
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
