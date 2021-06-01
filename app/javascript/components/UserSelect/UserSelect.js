36be01a287947a0250ece0cd3e88187a2fc12bf4
Kid-Guru <birkov.alexandr@gmail.com>
2021-06-01 14:01:39 +0400

    delete control focus

--- app/javascript/components/UserSelect/UserSelect.js
+++ app/javascript/components/UserSelect/UserSelect.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import AsyncSelect from 'react-select/async';
@@ -12,14 +12,13 @@ import UserPresenter from 'presenters/UserPresenter';
 import useStyles from './useStyles';
 
 const UserSelect = ({ error, label, isClearable, isDisabled, isRequired, onChange, value, helperText }) => {
-  const [isFocused, setFocus] = useState(false);
   const styles = useStyles();
   const handleLoadOptions = (inputValue) =>
     UsersRepository.index({ q: { firstNameOrLastNameCont: inputValue } }).then(({ data }) => data.items);
 
   return (
     <>
-      <FormControl margin="dense" disabled={isDisabled} focused={isFocused} error={error} required={isRequired}>
+      <FormControl margin="dense" disabled={isDisabled} error={error} required={isRequired}>
         <InputLabel shrink>{label}</InputLabel>
         <div className={styles.select}>
           <AsyncSelect
@@ -32,8 +31,6 @@ const UserSelect = ({ error, label, isClearable, isDisabled, isRequired, onChang
             isClearable={isClearable}
             defaultValue={value}
             onChange={onChange}
-            onFocus={() => setFocus(true)}
-            onBlur={() => setFocus(false)}
             menuPortalTarget={document.body}
             styles={{ menuPortal: (base) => ({ ...base, zIndex: 9999 }) }}
           />
@@ -52,7 +49,7 @@ UserSelect.propTypes = {
   isRequired: PropTypes.bool.isRequired,
   onChange: PropTypes.func.isRequired,
   value: PropTypes.shape(),
-  helperText: PropTypes.string.isRequired,
+  helperText: PropTypes.string,
 };
 
 UserSelect.defaultProps = {
