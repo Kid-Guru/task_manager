1391b150ad273b00ad7007e4409649da8b657223
Kid-Guru <birkov.alexandr@gmail.com>
2021-06-16 11:04:47 +0400

    connect redux to project

--- app/javascript/components/TaskBoard/TaskBoard.js
+++ app/javascript/components/TaskBoard/TaskBoard.js
@@ -121,7 +121,7 @@ const TaskBoard = () => {
   const handleTaskCreate = (params) => {
     const attributes = TaskForm.attributesToSubmit(params);
     return TasksRepository.create(attributes).then(({ data: { task } }) => {
-      loadColumnInitial(TaskPresenter.state(task));
+      Initial(TaskPresenter.state(task));
       handleClose();
     });
   };
